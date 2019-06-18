3088179a4f0ff83088d5d8a28bd7f908d5aae55a
prusche <prusche@gmail.com>
2019-06-17 21:34:14 -0700

    added author create/update/delete views

--- catalog/views.py
+++ catalog/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import RenewBookForm
 from .models import Book, Author, BookInstance, Subject, Language
 
@@ -99,4 +100,15 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
-    
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    # initial = {'date_of_death': '05/01/2018'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
